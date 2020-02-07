1fdd8d133831b93c9079ec1ea267c6a175bbc3a2
spensation <spencevans@gmail.com>
2020-02-07 15:48:38 -0500

    model specs passed

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
 	validates :title, presence: true
-	validates :release_year, uniqueness: true 
-	validates :release_year, presence: true, if: :released
+	validates :release_year, uniqueness: true, presence: true, if: :released, numericality: { less_than_or_equal_to: 2020 }
+	
 
 
 
